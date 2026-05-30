cf1c09871f8e7664cfc4bbe0b243d7482a967791
nikden <nikden@users.noreply.github.com>
2026-05-30 13:39:24 +0300

    Upgrade Astro and Tailwind stack

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://smirnov-ortho.ru",
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
